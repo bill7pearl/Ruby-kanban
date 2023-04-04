394ec94e38f44b0e287f28bede2e551f7d0844f9
Salwa99 <s.ritage99@gmail.com>
2023-04-04 21:33:18 +0000

    Initialize variables in class musicalbum

--- genre.rb
+++ genre.rb
@@ -1,2 +1,2 @@
-class genre
+class Genre
 end
--- music.rb
+++ music.rb
@@ -1,2 +1,15 @@
+require_relative 'item'
+require_relative 'genre'
+
 class MusicAlbum < Item
+    attr_accessor :name, :genres, :publish_date, :on_spotify
+  attr_writer :genre
+
+  def initialize(name, publish_date, on_spotify: false, genres: [])
+    super()
+    @name = name
+    @publish_date = Date.parse(publish_date)
+    @on_spotify = on_spotify
+    @genres = genres
+  end
 end
