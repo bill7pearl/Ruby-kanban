d45158a5009029b88dcdc1a890d8de4aea6c3bb3
Salwa99 <s.ritage99@gmail.com>
2023-04-04 22:56:02 +0000

    Create album table

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,3 +1,18 @@
 ------------ Create database ----------------
 CREATE DATABASE my_catalog;
 
+------------ Create music album table ----------------
+CREATE TABLE music_albums (
+  id SERIAL PRIMARY KEY,
+  publish_date DATE NOT NULL DEFAULT DATE,
+  on_spotify BOOLEAN,
+  archived BOOLEAN,
+  genre_id INT,
+  label_id INT,
+  author_id INT,
+  CONSTRAINT fk_genre FOREIGN KEY (genre_id) REFERENCES genres(genre_id),
+  CONSTRAINT fk_label FOREIGN KEY (label_id) REFERENCES label(label_id),
+  CONSTRAINT fk_author FOREIGN KEY (author_id) REFERENCES authors(author_id),
+  PRIMARY KEY(id)
+);
+
