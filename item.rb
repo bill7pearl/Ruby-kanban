23d0535500c589b29f3e943a74b99f16072059b4
Salwa99 <s.ritage99@gmail.com>
2023-04-04 21:04:28 +0000

    Add initialize method

--- item.rb
+++ item.rb
@@ -1,4 +1,15 @@
 require 'date'
 
 class Item
+    attr_reader :id, :genre, :author, :source, :label, :publish_date, :archived
+
+  def initialize(id = Random.rand(1..1000))
+    @id = id
+    @genre = nil
+    @author = nil
+    @source = nil
+    @label = nil
+    @publish_date = nil
+    @archived = false
+  end
 end
