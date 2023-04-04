35c0cdfaa14f7b940fc9a7acf1842a02f0c2787f
Salwa99 <s.ritage99@gmail.com>
2023-04-04 21:10:53 +0000

    Create console app entry point

--- app.rb
+++ app.rb
@@ -1,3 +1,22 @@
 class App
-    
-end 
+    def main_menu
+        app = App.new
+        puts "Welcome to our catalog 👋\nSelect an option: 👉"
+        options = [
+          '+++++++++++++++++++++++++++++++++++++++++++++++',
+          '1 - List books options',
+          '***********************************************',
+          '2 - List music options',
+          '***********************************************',
+          '3 - List game options',
+          '***********************************************',
+          '4 - Quit',
+          '+++++++++++++++++++++++++++++++++++++++++++++++',
+        ]
+        puts options
+        option = gets.chomp.downcase
+    end
+end
+
+app = App.new
+app.main_menu
