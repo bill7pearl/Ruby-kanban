1ef6f12046243aeddfedc9f79048cbf9262371c5
Salwa99 <s.ritage99@gmail.com>
2023-04-04 21:42:33 +0000

    Implement list all albums method

--- music_album_display.rb
+++ music_album_display.rb
@@ -11,4 +11,15 @@ class MusicAlbumDisplay
         puts '3. Add an album'
         puts '4. Quit'
       end
+      def list_all_albums
+        if @albums.empty?
+          puts 'No albums found! 🙁'
+        else
+          puts "***************** Album Information 🎧 ********************\n"
+          @albums.each do |album|
+            puts "--> Album title: #{album.name}\n--> Released year: #{album.publish_date.year}\n--> Music genre: #{album.genres}\n"
+            puts "===========================================================\n"
+          end
+        end
+      end
 end
