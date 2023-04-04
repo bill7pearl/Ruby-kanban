370df96eb25abb9399f34ec5fa40516e958f6f6a
Salwa99 <s.ritage99@gmail.com>
2023-04-04 21:41:54 +0000

    Implement display menu

--- music_album_display.rb
+++ music_album_display.rb
@@ -3,4 +3,12 @@ class MusicAlbumDisplay
         @filename = filename
         @albums = load_data || []
       end
+      
+      def display_menu
+        puts "Welcome! 👋😃\nChoose an option: 👉"
+        puts '1. List all albums'
+        puts '2. List all genres'
+        puts '3. Add an album'
+        puts '4. Quit'
+      end
 end
