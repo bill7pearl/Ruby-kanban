06521def96845896a6178626efb5b4ea47a13816
Salwa99 <s.ritage99@gmail.com>
2023-04-04 21:11:56 +0000

    Fix linter errors

--- app.rb
+++ app.rb
@@ -1,21 +1,19 @@
 class App
-    def main_menu
-        app = App.new
-        puts "Welcome to our catalog 👋\nSelect an option: 👉"
-        options = [
-          '+++++++++++++++++++++++++++++++++++++++++++++++',
-          '1 - List books options',
-          '***********************************************',
-          '2 - List music options',
-          '***********************************************',
-          '3 - List game options',
-          '***********************************************',
-          '4 - Quit',
-          '+++++++++++++++++++++++++++++++++++++++++++++++',
-        ]
-        puts options
-        option = gets.chomp.downcase
-    end
+  def main_menu
+    puts "Welcome to our catalog 👋\nSelect an option: 👉"
+    options = [
+      '+++++++++++++++++++++++++++++++++++++++++++++++',
+      '1 - List books options',
+      '***********************************************',
+      '2 - List music options',
+      '***********************************************',
+      '3 - List game options',
+      '***********************************************',
+      '4 - Quit',
+      '+++++++++++++++++++++++++++++++++++++++++++++++',
+    ]
+    puts options
+  end
 end
 
 app = App.new
