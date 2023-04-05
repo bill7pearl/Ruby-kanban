696e08043db93da932f76e34bb052a72a4078de2
Billal <billal7chami@gmail.com>
2023-04-05 04:11:28 +0100

    Added Book table & Labels table to database schema

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,6 +1,25 @@
 ------------ Create database ----------------
 CREATE DATABASE my_catalog;
 
+--------- Create book table ----------------
+CREATE TABLE books (
+  id SERIAL PRIMARY KEY,
+  publisher VARCHAR,
+  cover_state VARCHAR,
+  genre_id INT NULL REFERENCES genres(id), 
+  author_id INT NULL REFERENCES authors(id),
+  label_id INT NULL REFERENCES labels(id),
+  publish_date DATE NOT NULL,
+  archived BOOLEAN NOT NULL
+);
+
+----------- Create labels table ------------------
+CREATE TABLE labels (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(50) NOT NULL,
+  color VARCHAR(50) NOT NULL
+);
+
 ------------ Create music album table ----------------
 CREATE TABLE music_albums (
   id SERIAL PRIMARY KEY,
