035e3c6ff865d36f90f440919a307b2cfafbee0c
Billal <billal7chami@gmail.com>
2023-04-05 04:08:11 +0100

    Added JSON to preserve data

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
+require_relative 'book'
+require_relative 'label'
+require_relative 'book_methods'
+require_relative 'music_album'
+require_relative 'music_album_store'
+require_relative 'game'
+require_relative 'game_author'
+require 'json'
+
 class App
   def main_menu
+    app = App.new
     puts "Welcome to our catalog 👋\nSelect an option: 👉"
     options = [
       '+++++++++++++++++++++++++++++++++++++++++++++++',
@@ -13,6 +23,25 @@ class App
       '+++++++++++++++++++++++++++++++++++++++++++++++'
     ]
     puts options
+    option = gets.chomp.downcase
+    case option
+    when '1'
+      book = BookMethods.new('./data/books.json')
+      book.run
+    when '2'
+      album = MusicAlbumStore.new('./data/albums.json')
+      album.run
+    when '3'
+      game = GameStore.new
+      game.display_menu
+    when '4'
+      puts 'Thanks for using 😘'
+      nil
+    else
+      puts 'Invalid choice ❌! Please try again.'
+      main_menu
+      return
+    end
   end
 end
 
